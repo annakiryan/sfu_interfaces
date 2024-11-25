d9831c9ddc5ec5ed9a7022784998674c657bfbb6
Анна Кирьян <annnakirunovskaya@gmail.com>
2024-11-25 13:26:02 +0700

    add 5lab

--- lab5/model/settings.py
+++
@@ -1,8 +0,0 @@
-class GameSettings:
-    def __init__(self):
-        self.board_size = 15  # Размер поля
-        self.starting_player = 'X'  # Кто ходит первым
-
-    def update_settings(self, size, starting_player):
-        self.board_size = size
-        self.starting_player = starting_player
